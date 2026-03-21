85244c3a062ffab5530fb091e2f0c1f9bc57f8fd
oguzhanaydiin <oguuzaydinn@gmail.com>
2026-03-21 21:48:31 +0100

    Friends lists and requests (#11)
    * friends modals
    
    * friends routes and ws
    
    * fix
    
    * deleting friends

--- src/routes.rs
+++ src/routes.rs
@@ -1,11 +1,15 @@
 use actix_web::web;
 
-use crate::handlers::{health, otp, users, ws};
+use crate::handlers::{friends, health, otp, users, ws};
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health::get_status))
     .route("/users", web::get().to(users::list_users))
     .route("/users/username", web::put().to(users::save_username))
+    .route("/friends", web::get().to(friends::list_friends))
+    .route("/friends/{username}", web::delete().to(friends::remove_friend))
+    .route("/friends/requests", web::post().to(friends::send_friend_request))
+    .route("/friends/requests/accept", web::post().to(friends::accept_friend_request))
     .route("/ws", web::get().to(ws::ws_index))
     .route("/otp/send", web::post().to(otp::send_email_otp))
     .route("/otp/validate", web::post().to(otp::validate_email_otp));
